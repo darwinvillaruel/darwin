57814c54fdd553766790e2685622fb3724b4ee26
Darwin <darwinvillaruel2215@gmail.com>
2024-09-11 02:21:36 +0800

    feat: add Projects section

---
+++ src/components/Projects.tsx
@@ -0,0 +1,13 @@
+export default function Projects() {
+  return (
+    <section>
+      <h4>SELECTED PROJECTS</h4>
+      <ul>
+        <li>Video Editor Portfolio Site</li>
+        <li>Chat App System</li>
+        <li>Mini Projects</li>
+        <li>Automations</li>
+      </ul>
+    </section>
+  );
+}
