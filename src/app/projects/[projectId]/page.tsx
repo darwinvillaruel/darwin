953ab61511df735866a12f0ebe18644c5e3a3651
Darwin <darwinvillaruel2215@gmail.com>
2024-12-17 16:01:14 +0800

    feat: add projects

--- src/app/projects/[projectId]/page.tsx
+++ src/app/projects/[projectId]/page.tsx
@@ -1,8 +1,14 @@
+import project from "@/lib/project";
+import Image from "next/image";
+import { IoGlobeOutline } from "react-icons/io5";
+
 export default function ProjectDetails({
   params,
 }: {
   params: { projectId: string };
 }) {
+  const projects = project.find((proj) => proj.slug === params.projectId);
+
   return (
     <main className="container flex flex-col justify-between min-h-screen py-5">
       <section>
@@ -19,9 +25,33 @@ export default function ProjectDetails({
           A developer with interest in building full stack applications and
           setting up automations to improve productivity.
         </p>
-        <p className="py-2 mt-5 border-t-2 border-gray-600 border-dotted lg:text-xl">
-          {params.projectId}
-        </p>
+        <div className="py-2 mt-5 border-t-2 border-gray-600 border-dotted lg:text-xl">
+          <h3 className="my-5 font-semibold">{projects?.title}</h3>
+          <a href={projects?.projectLink}>
+            <IoGlobeOutline size={25} />
+          </a>
+          <p>{projects?.description}</p>
+          <Image
+            src={projects.media}
+            width={600}
+            height={600}
+            alt="video-porfolio"
+            className="my-3"></Image>
+          <div className="my-3">
+            <h5 className="mt-5 font-semibold">Technologies</h5>
+            {projects?.technologies.map((item, index) => (
+              <li key={index}>{item}</li>
+            ))}
+          </div>
+          <div className="mb-5">
+            <h5 className="font-semibold">Challenges Faced</h5>
+            <p>{projects?.challengesFaced}</p>
+          </div>
+          <div>
+            <h5 className="font-semibold">Future Improvements</h5>
+            <p>{projects?.futureImprovements}</p>
+          </div>
+        </div>
       </section>
     </main>
   );
--- src/lib/project.ts
+++ src/lib/project.ts
@@ -58,20 +58,6 @@ const project: Projects[] = [
     challengesFaced: "Routing the correct examination route per subjects.",
     futureImprovements: "Add reviewers for different licensure examination.",
   },
-  {
-    id: 3,
-    title: "Node CLI",
-    slug: "node-cli",
-    description:
-      "A browser-based Tic-Tac-Toe game supporting two-player functionality.",
-    technologies: ["JavaScript (ES6)", "HTML", "CSS"],
-    skillsDemonstrated: ["Game logic", "DOM manipulation", "Event handling"],
-    projectLink: "https://example.com/tic-tac-toe",
-    sourceCode: "https://github.com/username/tic-tac-toe",
-    media: "https://example.com/tic-tac-toe-screenshot.png",
-    challengesFaced: "Creating an AI that plays against the user.",
-    futureImprovements: "Add score tracking and a difficulty level for AI.",
-  },
 ];
 
 export default project;
