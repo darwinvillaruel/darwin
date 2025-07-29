4ea3167c746eed1e730d1f367ff1483504c84029
Darwin <darwinvillaruel2215@gmail.com>
2025-07-29 23:53:47 +0800

    desc: changes in the summary

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,15 +10,24 @@ export default function Home() {
           👋 Hi! I'm Darwin
         </h1>
         <p className="lg:text-lg font-space">
-          🧑‍💻 Data Analyst at{" "}
+          Data Analyst @{" "}
           <span className="px-2 bg-green-500">
             <a href="https://methodfi.com">Method</a>
-          </span>
+          </span>{" "}
+          & Integration Developer @{" "}
+          <span className="px-2 bg-blue-500">
+            <a href="https://uleads.com.au">Uleads</a>
+          </span>{" "}
         </p>
-        <p className="mt-2 text-sm lg:mt-5 lg:text-xl">
-          A developer and data analyst with interest in building full stack
-          applications and setting up automations to improve productivity.
-          Experienced in analyzing data to uncover{" "}
+        <p className="mt-2 text-sm lg:mt-5 lg:text-lg">
+          Four years of experience in building automation workflows and
+          integration systems across sales, marketing, CRM, and backend
+          platforms using tools like n8n, Zapier, Make.com, Hightouch, Leadbyte,
+          Leadshook and CRM systems like Zoho, Salesforce, Close, LendCRM,
+          Pipedrive, etc.
+          <br></br> <br></br> Works as data analyst and developer with interest
+          in building full stack applications and setting up automations to
+          improve productivity. Experienced in analyzing data to uncover{" "}
           <i>
             actionable insights, creating reports, and developing data-driven
             solutions
